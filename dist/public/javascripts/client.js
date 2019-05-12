747bb1c9328a9d6b148011ee01ce3317cb468761
ahmet-esmer <aesmerr@gmail.com>
2019-05-13 00:47:10 +0200

    .gitignore

--- dist/public/javascripts/client.js
+++
@@ -1,5 +0,0 @@
-function displayTotalPerPerson(sClientSideMessage) {
-    var message = sClientSideMessage;
-    document.getElementById("spanClientSideMessage").innerText = message;
-}
-//# sourceMappingURL=client.js.map
--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/server.ts
-var app_1 = require("./app");
-var PORT = 3000;
-app_1.default.listen(PORT, function () {
-    console.log('Express server listening on port ' + PORT);
-});
-//# sourceMappingURL=server.js.map
